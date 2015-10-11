377a847c20773c76b66efb603c8f5984b0f1061b
Xander Victory <x@xandervictory.id.au>
2015-10-11 14:41:01 +0800

    Add more entity CharRefs to make sure we're not borking the trimming

--- test/issue-150.js
+++ test/issue-150.js
@@ -1,9 +1,9 @@
 require(__dirname).test({
-  xml: '<r>&#x0;</r>',
+  xml: '<r>&#x0; &#x0000; &#60; &#x3c; &#00060; &#x0003c;</r>',
   strict: true,
   expect: [
     ['opentag', {'name':'r', attributes:{}, isSelfClosing: false}],
-    ['text', '\0'],
+    ['text', '\0 \0 < < < <'],
     ['closetag', 'r']
   ]
 });
